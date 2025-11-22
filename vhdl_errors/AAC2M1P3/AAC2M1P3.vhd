29074cfcbf1075298f858366a9e6be0d421d2e18
JLJ-V14 <jlopezjodar@gmail.com>
2025-11-22 21:51:26 +0100

    vhdl errors & majority_vote final version

--- vhdl_errors/AAC2M1P3/AAC2M1P3.vhd
+++ vhdl_errors/AAC2M1P3/AAC2M1P3.vhd
@@ -43,14 +43,14 @@ library ieee;                                -- line 1
 use ieee.std_logic_1164.all;                 -- line 2
                                              -- line 3
 entity find_errors is port (                      -- line 4
-    a: in bit_vector(0 to 3);                   -- line 5
+    a: in bit_vector(3 downto 0);                   -- line 5
     b: out std_logic_vector(3 downto 0);         -- line 6
     c: in bit_vector(5 downto 0));           -- line 7
 end find_errors;                             -- line 8
                                              -- line 9
 architecture not_good of find_errors is         -- line 10
   begin                                      -- line 11
-  my_label: process (c,a)                         -- line 12
+  my_label: process (a,c)                         -- line 12
     begin                                    -- line 13
     if c = "111111" then  								-- line 14 Hint: select 111111 for c value
       b <= to_stdlogicvector(a);                                -- line 15
